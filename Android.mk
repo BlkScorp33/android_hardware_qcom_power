9e08f24c8b99d1d0e1dc85cf4a9c6ce474f2eb0c
blackscorpion3 <don.berzley@gmail.com>
2017-09-27 07:23:12 -0400

    Use device_qcom_power instead of hardware_qcom_power

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
-  ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
     LOCAL_PATH := $(call my-dir)
 
     # HAL module implemenation stored in
